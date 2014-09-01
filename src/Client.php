9624314871f8d6e57643efd7b79c573fbfa48e0c
Sergei Morozov <morozov@tut.by>
2014-09-01 18:48:03 +0300

    Draft authentication

--- src/Client.php
+++ src/Client.php
@@ -3,20 +3,27 @@
 namespace Clerk;
 
 use GuzzleHttp\Client as HttpClient;
+use IDCT\FileArrayCache as Cache;
 
 class Client
 {
-    private $userId = '5c85f96c-2226-c8a0-9079-4e9e9acb0c8d';
-    private $oAuthToken = '422d3d7e-3b61-7d08-4c5b-54004b14efa9';
     private $httpClient;
+    private $cache;
+    private $username;
+    private $password;
 
-    public function __construct(HttpClient $httpClient, $username, $password)
+    public function __construct(HttpClient $httpClient, Cache $cache, $username, $password)
     {
         $this->httpClient = $httpClient;
+        $this->cache = $cache;
+        $this->username = $username;
+        $this->password = $password;
     }
 
     public function send(Timesheet $timesheet)
     {
+        $this->getAuthData($userId, $oAuthToken);
+
         $this->httpClient->post(array(
             '/rest/v10/Tasks/{task_id}/link/tasks_ps_timesheets_1',
             array(
@@ -24,13 +31,13 @@ class Client
             ),
         ), array(
             'headers' => array(
-                'OAuth-Token' => $this->oAuthToken,
+                'OAuth-Token' => $oAuthToken,
             ),
-            'body' => $this->format($timesheet),
+            'body' => $this->format($timesheet, $userId),
         ));
     }
 
-    protected function format(Timesheet $timesheet)
+    protected function format(Timesheet $timesheet, $userId)
     {
         $activity = $timesheet->getActivity();
         $subject = $timesheet->getSubject();
@@ -40,11 +47,57 @@ class Client
         return array(
             'activity_type' => $activity->getName(),
             'tasks_ps_timesheets_1tasks_ida' => $activity->getTask()->getId(),
-            'assigned_user_id' => $this->userId,
+            'assigned_user_id' => $userId,
             'name' => $name,
             'description' => $subject,
             'activity_date' => $date->format('Y-m-d'),
             'time_spent' => $timesheet->getTimeSpent(),
         );
+    }
+
+    private function getAuthData(&$userId, &$oAuthToken)
+    {
+        if (0 && isset($this->cache['user_id'], $this->cache['token'])) {
+            $userId = $this->cache['user_id'];
+            $oAuthToken = $this->cache['token'];
+        } else {
+            $this->authenticate($userId, $oAuthToken);
+            $this->cache['user_id'] = $userId;
+            $this->cache['token'] = $oAuthToken;
+        }
+    }
+
+    private function authenticate(&$userId, &$oAuthToken)
+    {
+        $oAuthToken = $this->getOAuthToken();
+        $userId = $this->getUserId($oAuthToken);
+    }
+
+    private function getOAuthToken()
+    {
+        $response = $this->httpClient->post('/rest/v10/oauth2/token', array(
+            'body' => array(
+                'grant_type' => 'password',
+                'username' => $this->username,
+                'password' => $this->password,
+                'client_id' => 'sugar',
+                'client_secret' => '',
+            ),
+        ));
+
+        $message = $response->json();
+        return $message['access_token'];
+    }
+
+    private function getUserId($oAuthToken)
+    {
+        $response = $this->httpClient->get('/rest/v10/me', array(
+            'headers' => array(
+                'OAuth-Token' => $oAuthToken,
+            ),
+        ));
+
+        $message = $response->json();
+        return $message['current_user']['id'];
     }
 }
